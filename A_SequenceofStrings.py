e74d97f959b5ec6cd016a46d48a0cfaf3652c843
S.Shiomi <shunsukeshiomi0914@gmail.com>
2023-03-09 19:04:27 +0900

    回答完了

--- A_SequenceofStrings.py
+++ A_SequenceofStrings.py
@@ -3,3 +3,8 @@ URL: https://atcoder.jp/contests/abc284/tasks/abc284_a
 AtCoder Beginner Contest 284
 A問題
 '''
+n = int(input())  # nは入力回数
+str_list = [input() for _ in range(n)]
+
+for i in range(n):
+    print(str_list[n-i-1])
