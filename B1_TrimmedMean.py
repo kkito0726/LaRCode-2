e8e15ffe5bebbdfb635bcbee6967212f1fdcc11e
S.Shiomi <shunsukeshiomi0914@gmail.com>
2023-03-09 19:40:21 +0900

    回答完了

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,13 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+
+n = int(input())  # nは入力回数
+num_list = [int(i) for i in input().split()]
+
+list_sort = sorted(num_list)
+all = len(num_list)
+
+average = sum(list_sort[n:all-n])/len(list_sort[n:all-n])
+print(average)
+
