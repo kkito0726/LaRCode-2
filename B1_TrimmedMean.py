77ff98531d68fb2f1405c286b1808cac52ac6d30
RedFieldf <momosuzy0216@gmail.com>
2023-04-25 16:05:07 +0900

    初めてのコミットです

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,16 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+N = int(input())
+X = list(map(int, input().split()))
+
+X.sort()
+
+ans = 0
+
+for i in range(N, 5*N-N):
+    ans += X[i]
+
+ave = ans/(3*N)
+print(ave)
+
