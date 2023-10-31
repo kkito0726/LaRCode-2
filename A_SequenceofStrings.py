364dd17eed4ffec63c25a5a9b36cc84cd47baa47
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-10-31 21:40:16 +0900

    A問題回答しました

--- A_SequenceofStrings.py
+++ A_SequenceofStrings.py
@@ -3,3 +3,9 @@ URL: https://atcoder.jp/contests/abc284/tasks/abc284_a
 AtCoder Beginner Contest 284
 A問題
 '''
+N = int(input())
+str_list = [input() for _ in range(N)]
+
+for i in range(N):
+    print(str_list[N-1])
+    N -= 1
