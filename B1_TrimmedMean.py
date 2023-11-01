35bc94af65449993213ba817f0515093ab00dff7
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-11-01 12:27:20 +0900

    B1問題解答完了

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,17 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+
+N = int(input())
+X = list(map(int, input().split()))
+
+score_list = sorted(X)
+for i in range(N):
+    del score_list[0]
+    del score_list[-1]
+total = 0
+a = 3*N
+for i in range(a):
+    total += score_list[i]
+average = total / a
+print(average)
