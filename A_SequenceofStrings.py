9279b3141870cfcbaf40d8ffb585ab755cf98676
takaaki <takaaki0210123@gmail.com>
2023-11-08 11:37:03 +0900

    A回答完了しました

--- A_SequenceofStrings.py
+++ A_SequenceofStrings.py
@@ -3,3 +3,9 @@ URL: https://atcoder.jp/contests/abc284/tasks/abc284_a
 AtCoder Beginner Contest 284
 A問題
 '''
+#入力
+N = int(input())
+name = [input() for _ in range(N)]
+#出力
+for i in range(N):
+    print(name[(N-1)-i])
