1806d599514a8a040c4c7d78b28234470c2ed2d0
takaaki <takaaki0210123@gmail.com>
2023-11-08 15:19:10 +0900

    B2回答完了しました

--- B2_YellowandRedCard.py
+++ B2_YellowandRedCard.py
@@ -3,3 +3,24 @@ URL: https://atcoder.jp/contests/abc292/tasks/abc292_b
 AtCoder Beginner Contest 292
 B問題
 '''
+#入力
+N,Q = map(int,input().split())
+event = []
+for i in range(Q):
+    event.append(list(map(int,input().split())))
+#出力
+#空のリスト作り
+j =[0 for _ in range(N)]
+#空のリストにそれぞれYellow,Redを入れてる
+for q in range(Q):
+    for p in range(N):
+        if event[q][1] == p+1:
+            if event[q][0] == 1:
+                j[p] += 1
+            if event[q][0] == 2:
+                j[p] += 2    
+    if event[q][0] == 3:
+            if j[event[q][1]-1] >= 2:
+                print('Yes')
+            else :
+                print('No')
