1812a87e9e606df60ecaa442edf8201eed51e2b9
takaaki <takaaki0210123@gmail.com>
2023-11-08 15:47:09 +0900

    B2回答完了しました

--- B2_YellowandRedCard.py
+++ B2_YellowandRedCard.py
@@ -10,17 +10,19 @@ for i in range(Q):
     event.append(list(map(int,input().split())))
 #出力
 #空のリスト作り
-j =[0 for _ in range(N)]
+j =[[] for _ in range(N)]
 #空のリストにそれぞれYellow,Redを入れてる
 for q in range(Q):
     for p in range(N):
         if event[q][1] == p+1:
             if event[q][0] == 1:
-                j[p] += 1
+                j[p].append('Yellow')
             if event[q][0] == 2:
-                j[p] += 2    
+                j[p].append('Red')    
     if event[q][0] == 3:
-            if j[event[q][1]-1] >= 2:
+            if len(j[event[q][1]-1]) == 2 or j[event[q][1]-1] == ['Red']:
                 print('Yes')
-            else :
+            else:
                 print('No')
+print(j)
+
