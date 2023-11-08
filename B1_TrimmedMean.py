165633221a71f906a6f6e76e7e240b6ea93eeb67
takaaki <takaaki0210123@gmail.com>
2023-11-08 11:44:31 +0900

    B1回答完了しました

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,13 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+#入力
+N = int(input())
+S =list(map(int, input(). split()))
+#出力
+for i in range(N):
+    S.remove(max(S))
+for n in range(N):
+    S.remove(min(S))
+S_sum = sum(S)
+print(S_sum/len(S))
