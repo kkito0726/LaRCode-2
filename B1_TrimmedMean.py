ca69479c980872590daaee7cd1651ca7da711c6a
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-21 16:19:17 +0900

    B1解答完了

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,11 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+import numpy as np
+N = int(input())
+num_list = list(map(int, input().split()))
+list_2 = sorted(num_list)
+del list_2[0:N]
+del list_2[-N:] 
+ave = np.mean(list_2)
+print(ave)
