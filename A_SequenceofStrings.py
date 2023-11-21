12ec47b4a9c5fdf144f31f9b0f2d0b0df88d9acd
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-21 15:24:15 +0900

    A問題解答完了

--- A_SequenceofStrings.py
+++ A_SequenceofStrings.py
@@ -3,3 +3,8 @@ URL: https://atcoder.jp/contests/abc284/tasks/abc284_a
 AtCoder Beginner Contest 284
 A問題
 '''
+N = int(input())
+tango = [input() for _ in range(N)]
+# print(tango)
+for i in reversed(tango):
+    print(i)
