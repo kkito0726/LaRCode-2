3b59e58f0ae48bf557e834185730bf044b87814c
moeno <moeno.furuya.5n@stu.hosei.ac.jp>
2023-12-01 14:23:57 +0900

    A解答完了

--- A_SequenceofStrings.py
+++ A_SequenceofStrings.py
@@ -3,3 +3,19 @@ URL: https://atcoder.jp/contests/abc284/tasks/abc284_a
 AtCoder Beginner Contest 284
 A問題
 '''
+n = int(input())
+str_list = [input() for _ in range(n)]
+# print(str_list)
+
+str_list.reverse()
+# print(str_list)
+
+# print(str_list[0])
+
+for num in str_list:
+    print(num)
+
+# for i in reversed(str_list):
+#     print(str_list)
+
+
