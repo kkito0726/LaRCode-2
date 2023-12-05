fb9c9100461ed7035668d7c821ebffb55c0f767d
moeno <moeno.furuya.5n@stu.hosei.ac.jp>
2023-12-05 15:18:27 +0900

    B1解答完了

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,21 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+
+N = int(input())
+tokuten = list(map(int, input().split()))
+
+# print(N)
+# print(tokuten)
+
+tokuten.sort()
+
+for i in range (N):
+    tokuten.pop()
+
+for i in range (N):
+    tokuten.pop(0)
+
+ave = sum(tokuten)/len(tokuten)
+
+print(ave)
