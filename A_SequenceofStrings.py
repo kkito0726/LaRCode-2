86c89efcadc6854df375602672d21ba0a9b414b8
conne <kanki1359@icloud.com>
2023-12-15 15:48:34 +0900

    A1　1回目

--- A_SequenceofStrings.py
+++ A_SequenceofStrings.py
@@ -3,3 +3,9 @@ URL: https://atcoder.jp/contests/abc284/tasks/abc284_a
 AtCoder Beginner Contest 284
 A問題
 '''
+n = int(input())  # nはint型
+str_list = [input() for _ in range(n)]
+
+
+for i in range(n-1, -1, -1) :
+    print(str_list[i])
