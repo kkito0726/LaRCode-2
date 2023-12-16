693b8741e400e6b719038712b101d21802483175
conne <kanki1359@icloud.com>
2023-12-16 19:58:27 +0900

    1回目

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,17 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+n = int(input())  # nはint型
+num_list = list(map(int, input().split()))  # n個の数字がリストに格納される
+
+
+ #最大値と最小値をN回ずつ削除
+for i in range(n):
+    
+    num_list.remove(max(num_list))
+    num_list.remove(min(num_list))
+
+# 平均を計算
+average = sum(num_list) / len(num_list)
+
+print(average)
