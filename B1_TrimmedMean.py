df918e05ab62c10b21cb64695dae987e03451a66
conne <kanki1359@icloud.com>
2023-12-19 19:18:21 +0900

    3回目

--- B1_TrimmedMean.py
+++ B1_TrimmedMean.py
@@ -3,3 +3,16 @@ URL: https://atcoder.jp/contests/abc291/tasks/abc291_b
 AtCoder Beginner Contest 291（Sponsored by TOYOTA SYSTEMS）
 B問題
 '''
+n = int(input())  # nはint型
+num_list = list(map(int, input().split()))  # n個の数字がリストに格納される
+
+for i in range(n) :
+
+
+  num_list.remove(max(num_list))
+  num_list.remove(min(num_list))
+
+
+score = sum(num_list) / len(num_list)
+
+print(score)
